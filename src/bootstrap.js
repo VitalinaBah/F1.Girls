bb331ff9862870d6d9761909247a1b7d3a2eb204
VitalinaBah <baktyn.vitalina@student.uzhnu.edu.ua>
2024-10-22 11:36:49 +0300

    new changes

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App';
+import App from './App.jsx';
 
 const domNode = document.querySelector('#react-app');
 const root = createRoot(domNode);

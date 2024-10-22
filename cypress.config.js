193b3cc10070b75281a11ec249682abe93463a6b
VitalinaBah <baktyn.vitalina@student.uzhnu.edu.ua>
2024-10-22 15:09:04 +0300

    new branch

--- cypress.config.js
+++ cypress.config.js
@@ -18,9 +18,5 @@ module.exports = defineConfig({
         },
     },
 
-    e2e: {
-        setupNodeEvents(on, config) {
-            // implement node event listeners here
-        },
-    },
+    
 });

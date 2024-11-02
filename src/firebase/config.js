ad6b44fb20b03c9ae59cf5c7513a341d2b9f53c1
VitalinaBah <baktyn.vitalina@student.uzhnu.edu.ua>
2024-11-02 21:43:56 +0200

    changed firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,14 +3,14 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyBs7B6Yz5Xb4Dk2ttDcrmLhEgyE8qIQbo4',
-    authDomain: 'intrest-64852.firebaseapp.com',
-    projectId: 'intrest-64852',
-    storageBucket: 'intrest-64852.appspot.com',
-    messagingSenderId: '787261982573',
-    appId: '1:787261982573:web:f462d9d7e52efc00ddbc64',
-    measurementId: 'G-GVPQ2SFFYV',
-};
+    apiKey: "AIzaSyCzYfAIQrJLeq00q4MKT6XB9ipjUd7qCnw",
+    authDomain: "f1girls-98385.firebaseapp.com",
+    projectId: "f1girls-98385",
+    storageBucket: "f1girls-98385.firebasestorage.app",
+    messagingSenderId: "339994420383",
+    appId: "1:339994420383:web:ee6e9b73544e0c5a53eeaa",
+    measurementId: "G-M78M2HP00M"
+  };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
